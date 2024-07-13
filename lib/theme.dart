eee994654cf38e84a706b57cb55d12b8dd63eba9
medch2023 <mohammed.achchamakmaki@gmail.com>
2024-07-13 18:40:46 +0100

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final kWhiteText = GoogleFonts.nunito(
+  color: kWhiteColor,
+);
+
+const kWhiteColor = Color(0xFFd9d9e3);
+const kPrimaryColor = Color.fromARGB(255, 192, 7, 78);
+
+const kBg500Color = Color.fromARGB(255, 54, 55, 56);
+const kBg300Color = Color(0xFF222528);
+const kBg100Color = Color(0xff272d31);
+
+const kMedium = FontWeight.w500;
+const kRegular = FontWeight.w400;
+const kSemiBold = FontWeight.w600;
